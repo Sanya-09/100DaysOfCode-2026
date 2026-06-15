2287aa5e45091fa6703f2e7f48004e0dd69f44f6
Sanya Agarwal <agarwalsanya90@gmail.com>
2026-06-15 23:50:54 +0530

    Update and rename DSA/SanyaAgarwal_500120145/Day_1/Question2/Solution.java to DSA/SanyaAgarwal_500120145/Day_1/Question2.java

--- DSA/SanyaAgarwal_500120145/Day_1/Question2.java
+++ DSA/SanyaAgarwal_500120145/Day_1/Question2.java
@@ -1,4 +1,4 @@
-package DSA.SanyaAgarwal_500120145.Day_1.Question2;
+package DSA.SanyaAgarwal_500120145.Day_1;
 public class Solution {
     public static void main(String[] args) {
         int[] arr = {2, 99, 1, 30, 54};
