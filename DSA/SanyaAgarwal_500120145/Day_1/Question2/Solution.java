2f312232ceaaca1cf11ca0b18e49387325d469d7
Sanya Agarwal <agarwalsanya90@gmail.com>
2026-06-15 23:44:11 +0530

    Update Solution.java

--- DSA/SanyaAgarwal_500120145/Day_1/Question2/Solution.java
+++ DSA/SanyaAgarwal_500120145/Day_1/Question2/Solution.java
@@ -1,10 +1,12 @@
 package DSA.SanyaAgarwal_500120145.Day_1.Question2;
 public class Solution {
     public static void main(String[] args) {
-        int[] arr = {12, 35, 1, 10, 34, 1};
+        int[] arr = {2, 99, 1, 30, 54};
         int sum = 0;
-        for (int num : arr) sum += num;
+        for (int num : arr) {
+            sum += num;
+        }
         System.out.println(sum);
     }
-}
+} 
 
